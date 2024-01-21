0c7e97f3bbae3c39620be6f73016e963848050b1
Abhijit Simha <22295630+simhaabhijit@users.noreply.github.com>
2024-01-21 08:12:49 +0530

    01 pure react in html with react cdn

---
+++ codingExercises.js
@@ -0,0 +1,36 @@
+// First React App - Demo/Example App
+
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces if advice
+    </p>
+  );
+}
