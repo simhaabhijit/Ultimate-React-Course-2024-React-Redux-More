3d583420cfd4b01733866f8f6277ef949d06b528
Abhijit Simha <22295630+simhaabhijit@users.noreply.github.com>
2024-01-31 06:13:46 +0530

    02 challenge Profile Card V2

--- challenges/01-profile-card/src/index.js
+++ challenges/01-profile-card/src/index.js
@@ -2,7 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
+const skills = [
+  { skill: "Flying Thunder God", level: "S-Rank", color: "burlywood" },
+  { skill: "Rasengan", level: "S-Rank", color: "lightblue" },
+  { skill: "Guiding Thunder ", level: "S-Rank", color: "blueviolet" },
+  { skill: "Sage Mode", level: "S-Rank", color: "orangered" },
+  { skill: "Shadow Clone", level: "A-Rank", color: "beige" },
+  { skill: "Uzumaki Reaper Death Seal", level: "S-Rank", color: "sienna" },
+  { skill: "Summoning", level: "A-Rank", color: "beige" },
+  { skill: "Sensing", level: "B-Rank", color: "beige" }
+];
+
 function App() {
+  const skillList = skills;
+
   return (
     <div className="card">
       <Avatar avatarImg="Minato.jpg" />
@@ -16,7 +29,7 @@ function App() {
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        <SkillList />
+        {skillList.length > 0 ? <SkillList skills={skillList} /> : null}
       </div>
     </div>
   );
@@ -37,24 +50,68 @@ function Intro(props) {
   );
 }
 
-function SkillList() {
+function SkillList({ skills }) {
+  // console.log(skills);
   return (
     <div className="skill-list">
-      <Skill name="Flying Thunder God" emoji="💨" color="burlywood" />
+      {/* <Skill name="Flying Thunder God" emoji="💨" color="burlywood" />
       <Skill name="Rasengan" emoji="💥" color="lightblue" />
       <Skill name="Space Time " emoji="🌀" color="blueviolet" />
       <Skill name="Sage Mode" emoji="🐸" color="orangered" />
       <Skill name="Shadow Clone" emoji="⚔️" color="beige" />
-      <Skill name="Uzumaki Death Reaper" emoji="🗡️" color="sienna" />
+      <Skill name="Uzumaki Death Reaper" emoji="🗡️" color="sienna" /> */}
+
+      {/* {skills.map((skill) => {
+        return < Skill skillObj={skill} key={skill.skill} />;
+      }
+      )} */}
+
+      {skills.map((skill) => <Skill skill={skill.skill} color={skill.color} level={skill.level} key={skill.skill} />)}
     </div>
   );
 }
 
-function Skill(props) {
+function getLevelEmoji(level) {
+  let emoji = "";
+  switch (level) {
+    case 'S-Rank':
+      emoji = "💥";
+      break;
+    case 'A-Rank':
+      emoji = "🔥";
+      break;
+    case 'B-Rank':
+      emoji = "⚔️";
+      break;
+    case 'C-Rank':
+      emoji = "🗡️";
+      break;
+    default:
+      emoji = "🌀";
+      break;
+  }
+
+  return emoji;
+}
+
+// function Skill({ skillObj }) {
+function Skill({ skill, level, color }) {
+
+  // const emoji = getLevelEmoji(skillObj.level);
+
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.name}</span>
-      <span>{props.emoji}</span>
+    // <div className="skill" style={{ backgroundColor: skillObj.color }}>
+    //   <span>{skillObj.skill}</span>
+    //   <span>{emoji}</span>
+    // </div>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === 'S-Rank' && "💥"}
+        {level === 'A-Rank' && "🔥"}
+        {level === 'B-Rank' && "⚔️"}
+        {level === 'C-Rank' && "🗡️"}
+      </span>
     </div>
   );
 }
