902657aef962d3e1302f63bcb6a07145b5966ffa
Abhijit Simha <22295630+simhaabhijit@users.noreply.github.com>
2024-02-08 16:19:27 +0530

    02 exercise accordion V2 - reusable comp's with children prop

--- exercises/02-accordion/src/App.js
+++ exercises/02-accordion/src/App.js
@@ -28,20 +28,48 @@ export default function App() {
 }
 
 function Accordion({ items }) {
+  const [currOpen, setCurrOpen] = useState(null);
+
   return (
     <div>
       {items.map((item, index) =>
-        <AccordionItem num={index} title={item.title} text={item.text} key={item.title} />
+        <AccordionItem
+          num={index}
+          title={item.title}
+          // text={item.text}
+          key={item.title}
+          currOpen={currOpen}
+          // onOpen={setCurrOpen} />
+          onOpen={setCurrOpen} >
+          {item.text}
+        </AccordionItem>
       )}
-    </div>
+      <AccordionItem
+        num={22}
+        title='Test 1'
+        key='test 1'
+        currOpen={currOpen}
+        onOpen={setCurrOpen} >
+        <p>Allow React Dev's to:</p>
+        <ul>
+          <li>break UI into Comp's</li>
+          <li>make Reusable comp's</li>
+          <li>plaace state efficiently</li>
+        </ul>
+      </AccordionItem>
+    </div >
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+// function AccordionItem({ num, title, text, currOpen, onOpen }) {
+function AccordionItem({ num, title, currOpen, onOpen, children }) {
+
+  // const [isOpen, setIsOpen] = useState(false);
+  const isOpen = (num === currOpen);
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    // setIsOpen((isOpen) => !isOpen);  
+    onOpen(isOpen ? null : num);
   }
 
   return (
@@ -49,8 +77,8 @@ function AccordionItem({ num, title, text }) {
       <p className='number'>{num < 9 ? `0${num + 1}` : num}</p>
       <p className='title'>{title}</p>
       <p className='icon'>{isOpen ? '-' : '+'}</p>
-
-      {isOpen && <div className='content-box'>{text}</div>}
+      {/* {isOpen && <div className='content-box'>{text}</div>} */}
+      {isOpen && <div className='content-box'>{children}</div>}
     </div>
   );
 }
